cc7be95445ec7851d3d1fbce6d83c06262de722b
Aleksa Nikolić <aleksa.x.nikolic@gmail.com>
2021-10-08 21:03:21 +0200

    Add favicon

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useSelector, useDispatch } from 'react-redux';
 import { setNavRefresh } from './store/navRefreshSlice';
 import styles from './Layout.module.css';
@@ -30,6 +31,15 @@ function Layout({ children }) {
 
   return (<>
     <div className={styles.container} onScroll={isTabletLand ? isTabletPort ? null : handleScroll : null }>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="favicon/android-chrome-512x512.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="favicon/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+        <link rel="icon" type="image/x-icon" href="favicon/favicon.ico" />
+        <link rel="manifest" href="favicon/site.webmanifest" />
+      </Head>
       <Navbar />
       <main className={styles.main}>{children}</main>
       <Footer />
